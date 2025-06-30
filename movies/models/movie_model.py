d6bcd5c3c42f922cb99d130d33f9bbab86ee7247
Dinesh Paudel <paudeldinesh961@gmail.com>
2025-06-30 09:16:51 +0545

    added

--- movies/models/movie_model.py
+++ movies/models/movie_model.py
@@ -19,7 +19,7 @@ class Movie(models.Model):
 class Review(models.Model):
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name="reviews")
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)])  # ⭐ 1-5 rating
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 6)], null=True, blank=True)
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
 
